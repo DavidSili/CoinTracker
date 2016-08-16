e30e7e8f36487427165a3541c99d022115b08105
DavidSili <agapetos@gmail.com>
2016-08-16 10:00:17 +0200

    Gotova prva faza
    Postigao sam da se redovno pokreće PHP fajl na serveru i unosi podatke u
    tabelu.

--- test.php
+++ test.php
@@ -6,3 +6,10 @@ $broj = rand(1,9999);
 $sql = 'INSERT INTO test (randomno) VALUES (:randomno)';
 $stmt=$db->prepare($sql);
 $stmt->execute(array(':randomno'=>$broj));
+
+/*
+ * U C-panel-u otići na Cron Jobs i izabrati "Add New Cron Job" sa željenim podešavanjima:
+ *
+ * /usr/local/bin/php -q /home/petrasil/etc/cron.5m/test.php > /dev/null
+ *
+ */
