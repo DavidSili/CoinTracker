084d45e55ed7af698816c4248e65535d7bac1ec4
DavidSili <agapetos@gmail.com>
2016-08-16 09:09:30 +0200

    Testovi su u toku
    Za sada isprobavam da Cron automatski pokreće ovaj fajl i unosi
    jednostavne podatke u bazu podataka.

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
 <?php
+require "connection.php";
+
+$db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8mb4', $user, $password);
+$broj = rand(1,9999);
+$sql = 'INSERT INTO test (randomno) VALUES (:randomno)';
+$stmt=$db->prepare($sql);
+$stmt->execute(array(':randomno'=>$broj));
